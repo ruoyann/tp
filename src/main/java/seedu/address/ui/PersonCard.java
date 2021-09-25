2951e008a30e5646871fcff027efd02fc9efd2d7
ruoyann <ruoyan@u.nus.edu>
2021-09-26 01:22:09 +0800

    Add remark command

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -27,6 +27,8 @@ public class PersonCard extends UiPart<Region> {
     public final Person person;
 
     @FXML
+    private Label remark;
+    @FXML
     private HBox cardPane;
     @FXML
     private Label name;
@@ -51,6 +53,7 @@ public class PersonCard extends UiPart<Region> {
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
+        remark.setText(person.getRemark().value);
         email.setText(person.getEmail().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
