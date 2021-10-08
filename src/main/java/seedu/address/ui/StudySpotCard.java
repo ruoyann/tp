18385f73dda80e333a62b1680ed4c329d35ea2ed
limdanqi <limdanqi01@gmail.com>
2021-10-09 00:24:38 +0800

    Update Amenities Gui (Part 1)
    Enhanced amenities such that the amenities shown for a study spot is in sync with it.

--- src/main/java/seedu/address/ui/StudySpotCard.java
+++ src/main/java/seedu/address/ui/StudySpotCard.java
@@ -1,12 +1,17 @@
 package seedu.address.ui;
 
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.shape.SVGPath;
+import seedu.address.model.amenity.Amenity;
 import seedu.address.model.studyspot.Rating;
 import seedu.address.model.studyspot.StudySpot;
 
@@ -16,6 +21,17 @@ import seedu.address.model.studyspot.StudySpot;
 public class StudySpotCard extends UiPart<Region> {
 
     private static final String FXML = "StudySpotListCard.fxml";
+
+    private static final String SVGPATH_WIFI_CONTENT =
+            "M634.91 154.88C457.74-8.99 182.19-8.93 5.09 154.88c-6.66 6.16-6.79 16.59-.35 22.98l34.24 33.97c6.14 "
+                    + "6.1 16.02 6.23 22.4.38 145.92-133.68 371.3-133.71 517.25 0 6.38 5.85 16.26 5.71 "
+                    + "22.4-.38l34.24-33.97c6.43-6.39 6.3-16.82-.36-22.98zM320 352c-35.35 0-64 28.65-64 64s28.65 64 64 "
+                    + "64 64-28.65 64-64-28.65-64-64-64zm202.67-83.59c-115.26-101.93-290.21-101.82-405.34 0-6.9 "
+                    + "6.1-7.12 16.69-.57 23.15l34.44 33.99c6 5.92 15.66 6.32 22.05.8 83.95-72.57 209.74-72.41 293.49 "
+                    + "0 6.39 5.52 16.05 5.13 22.05-.8l34.44-33.99c6.56-6.46 6.33-17.06-.56-23.15z";
+    private static final String SVGPATH_CHARGER_CONTENT = "M320,32a32,32,0,0,0-64,0v96h64Zm48,128H16A16,16,0,0,0,0,"
+            + "176v32a16,16,0,0,0,16,16H32v32A160.07,160.07,0,0,0,160,412.8V512h64V412.8A160.07,160.07,0,0,0,352,"
+            + "256V224h16a16,16,0,0,0,16-16V176A16,16,0,0,0,368,160ZM128,32a32,32,0,0,0-64,0v96h64Z";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -41,6 +57,8 @@ public class StudySpotCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
+    @FXML
+    private FlowPane amenities;
 
     /**
      * Creates a {@code StudySpotCode} with the given {@code StudySpot} and index to display.
@@ -56,6 +74,9 @@ public class StudySpotCard extends UiPart<Region> {
         studySpot.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        studySpot.getAmenities().stream()
+                .sorted(Comparator.comparing(amenity -> amenity.amenityType))
+                .forEach(amenity -> amenities.getChildren().add(getAmenityLabel(amenity.amenityType)));
     }
 
     @Override
@@ -74,6 +95,28 @@ public class StudySpotCard extends UiPart<Region> {
         StudySpotCard card = (StudySpotCard) other;
         return id.getText().equals(card.id.getText())
                 && studySpot.equals(card.studySpot);
+    }
+
+    private Label getAmenityLabel(String amenityType) {
+        Label result = new Label();
+
+        SVGPath icon = new SVGPath();
+        icon.setScaleX(0.03);
+        icon.setScaleY(0.03);
+        icon.getStyleClass().add("svg_icon");
+        icon.getStyleClass().add("cell_muted_label");
+        switch (amenityType) {
+        case "wifi":
+            icon.setContent(SVGPATH_WIFI_CONTENT);
+            result.setGraphic(icon);
+            return result;
+        case "charger":
+            icon.setContent(SVGPATH_CHARGER_CONTENT);
+            result.setGraphic(icon);
+            return result;
+        default:
+            throw new AssertionError("error occured");
+        }
     }
 
     private String ratingDisplay(Rating providedRating) {
