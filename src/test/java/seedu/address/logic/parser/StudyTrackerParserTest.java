7ccb0a8d6f69dd1b92bc70a647ecd417595fb0f6
ruoyann <68816578+ruoyann@users.noreply.github.com>
2021-10-10 23:47:33 +0800

    Update StudyTrackerParserTest.java

--- src/test/java/seedu/address/logic/parser/StudyTrackerParserTest.java
+++ src/test/java/seedu/address/logic/parser/StudyTrackerParserTest.java
@@ -6,7 +6,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SPOT;
 import static seedu.address.testutil.TypicalStudySpots.STARBUCKS;
 
 import java.util.Arrays;
