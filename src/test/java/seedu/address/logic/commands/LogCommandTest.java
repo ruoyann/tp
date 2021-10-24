99a1e1450ab4830443a9bfef5792114a69087cbd
ruoyann <ruoyan@u.nus.edu>
2021-10-24 23:19:51 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/LogCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LogCommandTest.java
@@ -1,20 +1,24 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.logic.commands.CommandTestUtil.*;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_AMENITY_AIRCON;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalStudySpots.getTypicalStudyTracker;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.*;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.ModelStub;
+import seedu.address.model.StudyTracker;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.studyspot.Name;
 import seedu.address.model.studyspot.StudiedHours;
 import seedu.address.model.studyspot.StudySpot;
-import seedu.address.testutil.EditStudySpotDescriptorBuilder;
 import seedu.address.testutil.StudySpotBuilder;
 
 /**
