1ca12553f7a781b4f7d976d295700b4796e824b7
ruoyann <ruoyan@u.nus.edu>
2021-10-24 20:40:33 +0800

    LogCommandTest: Fix Checkstyle

--- src/test/java/seedu/address/logic/commands/LogCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LogCommandTest.java
@@ -2,36 +2,15 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_DECK;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_FRONTIER;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_AMENITY_AIRCON;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_AMENITY_CHARGER;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_AMENITY_FOOD;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_AMENITY_WIFI;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_DECK;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_RATING_DECK;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_COFFEE;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_COLD;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_QUIET;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showStudySpotAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SPOT;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_SPOT;
 import static seedu.address.testutil.TypicalStudySpots.getTypicalStudyTracker;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.StudyTracker;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.studyspot.Name;
 import seedu.address.model.studyspot.StudiedHours;
-import seedu.address.model.studyspot.StudySpot;
-import seedu.address.testutil.StudySpotBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for LogCommand.
@@ -49,7 +28,6 @@ public class LogCommandTest {
 
         // same values -> returns true
         Name secondStudySpotInTypicalStudySpots = new Name("Central library");
-//        StudiedHours copyStudiedHours = new StudiedHours(String.valueOf(studiedHours.getHours()));
         StudiedHours copyStudiedHours = new StudiedHours("4");
         StudiedHours differentStudiedHours = new StudiedHours("7");
         LogCommand commandWithSameValues = new LogCommand(firstStudySpotInTypicalStudySpots, copyStudiedHours,
